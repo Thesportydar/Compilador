75ab5a2b6f11f13ac77f9fcc01befc49c481f19d
Iñaqui Paladino <76879494+Thesportydar@users.noreply.github.com>
2023-10-04 23:02:51 +0000

    Delete target/classes/compi directory

--- target/classes/compi/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#copy AS0.java to AS1..AS10, change all the ocurrencies of AS0 to AS1..AS10
-for i in {1..9}
-do
-    cp AS0.java AS$i.java
-    gsed -i 's/AS0/AS'$i'/g' AS$i.java
-done
